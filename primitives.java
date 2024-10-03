4b7b72a999bae78fd2d2464c4b59d99d880e4442
paraggarg17 <paraggarg1718@gmail.com>
2024-10-04 00:12:25 +0530

    g

---
+++ primitives.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class primitives {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter the input");
+        int rollno =input.nextInt();
+        System.out.println("your rollno is"+rollno);
+        float marks  =input.nextInt();
+        System.out.println("your marks is :"+marks);
+        
+
+
+        
+    }
+    
+}
