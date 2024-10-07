0c46990b7f723481338d660709ac073d57a5f37b
paraggarg17 <paraggarg1718@gmail.com>
2024-10-08 01:40:39 +0530

    ff

---
+++ pre.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class pre {
+    Scanner sc = new Scanner(System.in);
+    int input = sc.nextint();
+    System.out.println(input);
+    float price =sc.nextfloat();
+    System.out.println(price);
+}
